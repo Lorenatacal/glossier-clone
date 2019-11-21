5e54bda6e92d2bcf91a636971d2c67120e9b534a
Lorena <lorena.tacal@gmail.com>
2019-11-21 23:22:23 +0000

    Update component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ import {
   Link } from "react-router-dom";
 import Products from './components/Products';
 import Nav from './components/Nav';
-import Basket from './components/Basket'
+import Basket from './components/Basket';
+import LipsProducts from './components/LipsProducts';
+import EyelinerProducts from './components/EyelinerProducts';
+import FoundationProducts from './components/FoundationProducts';
+import NailPolish from './components/NailPolish';
 
 
 function App() {
@@ -15,7 +19,19 @@ function App() {
       <Router>
         <Nav />
           <Switch>
-            <Route exact path="/products">
+          <Route path="/LipsProducts">
+              <LipsProducts />
+            </Route>
+          <Route path="/EyelinerProducts">
+            <EyelinerProducts />
+          </Route>
+          <Route path="/FoundationProducts">
+            <FoundationProducts />
+          </Route>
+          <Route path="/NailPolish">
+            <NailPolish />
+          </Route>
+          <Route exact path="/products">
               <Products />
             </Route>
             <Route path="/Basket">
