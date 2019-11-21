2a796a7f8a67047e9f289cc9adead714f922afd6
Lorena <lorena.tacal@gmail.com>
2019-11-21 23:17:43 +0000

    Styled Checkout Component

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,32 +1,35 @@
 import React from 'react';
 
 function Checkout() {
-console.log('some')
     return(
-        <form className="checkoutButton">
-            <p>Name:</p>
-                <input
-                    type="text"
-                />
-            <p>Delivery Address</p>
-            <input
-                    type="text"
-            />
-            <p>Credit Card Details:</p>
-                <input
-                    type="text"
-                />
-            <p>Expiring Date:</p>
-                <input
-                    type="text"
-            />
-            <p>CVC:</p>
-                <input
-                    type="text"
-                />
+        <div className="formConatiner">
+            <form className="form">
+                <p>Please complete your details bellow:</p>
+                <label>
+                    <input className="inputForm" type="text" placeholder="Name:"/>
+                </label>
+                <label>
+                    <input className="inputForm" type="text" placeholder="Delivery Address:"/>
+                </label>
+                <label>
+                    <input className="inputForm" id="zip" name="zip" type="number" placeholder="Post Code:"/>
                 <br />
-            <button className="checkoutButton">Submit your order</button>
-      </form>
+                </label>
+                
+                <label>
+                <br />
+                    <input className="inputForm" id="zip" name="zip" type="number" placeholder="Credit Card Details:"/>
+                </label>
+                <label>
+                    <input className="inputForm" type="text" placeholder="Expiring Date:"/>
+                </label>
+                <label>
+                    <input className="inputForm" type="text" placeholder="CVC:"/>
+                </label>
+                    <br />
+                <button className="checkoutButton">Submit your order</button>
+        </form>
+      </div>
     )
     
 }
