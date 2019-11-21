7af45fa8ca71c7fcaeeac80dce1d36abc2503d7d
Lorena <lorena.tacal@gmail.com>
2019-11-21 23:21:33 +0000

    Update component

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import AllProducts from './AllProducts';
+import LipsProducts from './LipsProducts';
+import EyelinerProducts from './EyelinerProducts';
+import FoundationProducts from './FoundationProducts';
+import NailPolish from './NailPolish';
 import { useSelector } from 'react-redux'
 
 function Products() {
+
   return (
     <div className="App">
       <AllProducts />
+      <LipsProducts />
+      <EyelinerProducts />
+      <FoundationProducts />
+      <NailPolish />
     </div>
   );
 }
