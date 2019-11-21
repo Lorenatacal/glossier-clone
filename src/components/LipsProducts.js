5ea9bd27ffc215b2f987326054fa7fa6065da717
Lorena <lorena.tacal@gmail.com>
2019-11-21 23:18:15 +0000

    Add lip products component

--- src/components/LipsProducts.js
+++ src/components/LipsProducts.js
@@ -4,50 +4,52 @@ import { useDispatch, useSelector } from "react-redux";
 import './AllProducts.css'
 import { createUpdateData } from '../redux';
 
-function AllProducts() {
+function LipsProducts() {
     const [data, setData] = React.useState(useSelector(state => state.data));
     const dispatch = useDispatch()
 
   
-    // React.useEffect(() => {
-    //   axios
-    //   .get('http://makeup-api.herokuapp.com/api/v1/products.json?product_type=lipstick')
-    //   .then(response => {
-    //     console.log(response, 'respLips')
-    //     setData(response.data)
-    //     dispatch(createUpdateData(response.data))
-    //   });
-    // }, [])
+    React.useEffect(() => {
+      axios
+      .get('http://makeup-api.herokuapp.com/api/v1/products.json?product_type=lipstick')
+      .then(response => {
+        setData(response.data)
+        dispatch(createUpdateData(response.data))
+      });
+    }, [])
   
     return (
       <div className="App">
-        <h1 className="title">Shop All Products</h1>
-        {/* <div className='cards'>
+        <h1 className="title">Shop Lips Products</h1>
+        <div className='cards'>
           {data && data.map(({ id, name, brand, price, image_link, description }) => {
             const obj = {
                   id,
                   name,
                   price,
                   image_link,
-                  brand
+                  brand,
+                  quantity: 1
                 }
             return (      
                 <div className='card'>
                   <p className="productName">{name}</p>
-                  <img src={image_link} alt="Product"/>
+                  <img width="158px" height="184px" src={image_link} alt="Product"/>
                   <p>{brand}</p>
                   <p>{price} £</p>
                   <button onClick={() => {
-                    dispatch({type: 'ADD_TO_BASKET' })
+                    dispatch({type: 'ADD_TO_BASKET', payload: obj})
+                    dispatch({type: 'INCREASE_NAV_BAR'})
+                    dispatch({type: 'ADD_TOTAL_PRICE'})
                   }}
                   >
                     Add To Basket</button>
                 </div>
             )
           })}
-        </div> */}
+        </div>
       </div>
     );
   }
   
-  export default AllProducts;
+  export default LipsProducts;
